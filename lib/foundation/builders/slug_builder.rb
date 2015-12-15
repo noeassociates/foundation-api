515f150511b3e566b616a9772a6c9e43c73a18df
Jack Jennings <jack@remote.gd>
2015-12-15 14:32:22 -0800

    Use a default name for missing name parameters

--- lib/foundation/builders/slug_builder.rb
+++ lib/foundation/builders/slug_builder.rb
@@ -4,7 +4,26 @@ class Foundation::Builders::SlugBuilder < Foundation::Builder
 
   def run params, env
     name = [params.first_name, params.last_name].join ' '
+    name = placeholder_name if name.blank?
     env.slug = name.parameterize '-'
   end
+
+  private
+
+    def placeholder_name
+      MissingName.new
+    end
+
+    class MissingName < String
+
+      def initialize
+        super('visitor')
+      end
+
+      def split(*)
+        raise NotImplementedError
+      end
+
+    end
 
 end
--- test/test_foundation_api.rb
+++ test/test_foundation_api.rb
@@ -137,6 +137,11 @@ class Foundation::APITest < Minitest::Test
     end
   end
 
+  def test_uses_default_name_if_names_are_blank
+    post "/v1/interest", valid_post_params.merge('first_name': '', 'last_name': '')
+    assert_equal 'visitor', JSON.parse(last_response.body)["slug"]
+  end
+
   protected
 
   def valid_post_params
